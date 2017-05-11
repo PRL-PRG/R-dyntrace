a034dcb5fe041aade9ed39add3f23dee17f638bf
Konrad Siek <konrad.siek@cs.put.edu.pl>
2017-05-11 16:28:26 -0400

    Benchmarking stuff.

---
+++ rdt-plugins/promises/R/benchmark.R
@@ -0,0 +1,25 @@
+# Run as: Rscript file argv
+
+#DIR="/data/kondziu/"
+DIR="./"
+
+packages = commandArgs(trailingOnly = TRUE)
+
+custom.executor <- function(executor, path)
+    function(expr, current_vignette, total_vignettes, vignette_name, vignette_package, ...) {
+        write(paste("Vignette ", (current_vignette + 1), "/", total_vignettes,
+                        " (", vignette_name, " from ", vignette_package, "), saving to '", path, "'", sep=""),
+            stderr())
+
+    executor(eval(expr), path=path, overwrite=if (current_vignette == 0) TRUE else FALSE, ...)
+}
+
+start <- proc.time()
+
+for (p in packages)
+    run.all.vignettes.from.package(p, custom.executor(trace.promises.db, path=paste(DIR, p, ".sqlite", sep="")))
+
+end <- proc.time()
+
+write("Elapsed time:", stderr())
+write(end - start, stderr())
---
+++ run_vignettes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CMD='bin/Rscript rdt-plugins/promises/R/benchmark.R'
+
+PACKAGES=
+
+if [ $# -ge 1 ]
+then
+    PACKAGES="$@"
+else 
+    PACKAGES="grid dplyr ggplot2 haven readr readxl stringr tibble tidyverse digest colorspace kernlab vcd rpart survival mclust party mvtnorm igraph"
+fi
+
+for i in "$@"
+do 
+    $CMD $i 
+done   
+
