4193db311b1362cedccbd3c3611caf75eb0777b2
Konrad Siek <konrad.siek@gmail.com>
2017-08-22 17:51:00 +0200

    Vignette summary

---
+++ vignette_summary.R
@@ -0,0 +1,56 @@
+library(dplyr)
+
+packages <- installed.packages()[, "Package"]
+
+#vignettes.in.package <- vignette(package = package)$results[,3]
+
+
+filter_irrelecant_code <- function(source) {
+  content <- source 
+  
+  index.el <- content %>% grepl("^[ \t]*$", .) # Remove empty lines
+  content <- content[!index.el]
+  
+  index.cm <- content %>% grepl("^#", .) # Remove comments
+  content <- content[!index.cm]
+  
+  content
+}
+
+count_source_lines <- function(paths) {
+  result <- 0
+  for (path in paths)
+    result <- result + length(filter_irrelecant_code(path))
+}
+
+package_vignette_summary <- function(vignette_summary_data=NA) {
+  #tibble(package = packages) %>% rowwise %>% mutate(n.vignettes=length(vignette(package = package)$results[, "Item"]))
+  data <- if(suppressWarnings(is.na(vignette_summary_data))) vignette_summary() else vignette_summary_data 
+  
+  data %>% group_by(package) %>% summarise(n.vignettes=sum(as.integer(!is.na(name))), n.lines=sum(ifelse(is.na(lines), 0, lines)))
+}
+
+vignette_summary <- function() {
+  result.vignettes <- tibble(index=list(), package=list(), name=list(), source=list(), lines=list())
+  first = TRUE
+  
+  for(package in packages) {
+    vignettes <- vignette(package = package)$results
+    
+    if (length(vignettes) == 0)
+      next
+    
+    vignettes.information <- 
+        tibble(index=1:length(vignettes[, "Item"]), package = vignettes[, "Package"], name = vignettes[, "Item"]) %>%
+        rowwise %>% mutate(source={vignette.info <- vignette(name, package = package);  file.path(vignette.info$Dir, "doc", vignette.info$R)}) %>%
+        mutate(lines = length(filter_irrelecant_code(readLines(source))))
+    
+    if (first) {
+      result.vignettes <- vignettes.information
+      first <- FALSE
+    } else 
+      result.vignettes <- rbind(result.vignettes, vignettes.information)
+  }
+  
+  tibble(package=packages) %>% left_join(result.vignettes, by="package")
+}
