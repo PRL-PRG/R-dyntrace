624402c0c43f905f0fe9813c9af20298a928910a
Aviral Goel <goel.aviral@gmail.com>
2017-08-23 13:20:36 -0400

    Fix size computation reported by probe_vector_alloc for R vectors of size 1

--- src/main/memory.c
+++ src/main/memory.c
@@ -2539,7 +2539,7 @@ SEXP allocVector3(SEXPTYPE type, R_xlen_t length, R_allocator_t *allocator)
 	    SET_NAMED(s, 0);
 	    INIT_REFCNT(s);
 #ifdef RDT_IS_ENABLED
-      RDT_HOOK(probe_vector_alloc, type, length, alloc_size * sizeof(VECREC), "");
+      RDT_HOOK(probe_vector_alloc, type, length, NODE_SIZE(type), "");
 #endif
 	    return(s);
 	}
